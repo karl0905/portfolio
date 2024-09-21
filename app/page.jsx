61ec8dcfea3406c30c9565f7fd5dfde18a4be869
karl0905 <kalle44@live.dk>
2024-09-21 19:39:05 +0200

    Added public/ dir and created favicon

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>hej</h1>
+    <>
+      <h1>hej</h1>
+      <Image src="/favicon.png" alt="favicon" height="20" width="20"/>
+    </>
   );
 }
