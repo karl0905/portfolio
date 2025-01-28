67950ba4efce06c8ef81066ddbfd8a949d2e2320
karl0905 <kalle44@live.dk>
2025-01-28 21:50:14 +0100

    minor styling changes

--- global/components/MainContent.jsx
+++ global/components/MainContent.jsx
@@ -10,16 +10,18 @@ export function MainContent() {
   const contentDescription = mainContent?.content;
 
   return (
-    <div className="flex flex-col px-4 pt-10 overflow-x-hidden overflow-y-auto w-full">
-      {contentDescription?.map((str, index) => (
-        <p
-          key={index}
-          className="text-md py-4"
-          dangerouslySetInnerHTML={{ __html: colorizeString(str) }}
-        />
-      ))}
+    <div className="flex flex-col px-4 pt-10 overflow-x-hidden overflow-y-auto w-full gap-12">
+      <div>
+        {contentDescription?.map((str, index) => (
+          <p
+            key={index}
+            className="text-md py-2"
+            dangerouslySetInnerHTML={{ __html: colorizeString(str) }}
+          />
+        ))}
+      </div>
       {snippet && (
-        <pre className="text-md py-4 border border-grey-400 p-4">
+        <pre className="text-sm py-4 border border-grey-400 p-4">
           <code dangerouslySetInnerHTML={{ __html: colorizeSnippet(snippet, snippetFileType) }} />
         </pre>
       )}
