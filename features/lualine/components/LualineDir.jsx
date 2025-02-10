dd2471f2f55e2a3e85cc2ce19d001c14baa003eb
karl0905 <kalle44@live.dk>
2025-02-10 10:15:39 +0100

    _fix: LualineDir fixed

--- features/lualine/components/LualineDir.jsx
+++ features/lualine/components/LualineDir.jsx
@@ -1,7 +1,7 @@
 export function LualineDir({ currentDir = { dir: "home/karl/projects/portfolio", file: "index" } }) {
   return (
     <p className="text-[var(--text)] opacity-40 text-sm pl-2">
-      {currentDir.dir && `${currentDir.dir}/`}{currentDir.file || "index"}{!currentDir.file.includes(".jsx") && `home.jsx`}
+      {currentDir.dir && `${currentDir.dir}/`}{currentDir.file || "index"}{!currentDir.file.includes(".jsx") && `.jsx`}
     </p>
   )
 }
