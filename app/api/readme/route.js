d96f9573281bfab51550a15958869bc2fe8ea8b2
karl0905 <kalle44@live.dk>
2025-02-10 14:07:35 +0100

    removed unused api routes

--- app/api/readme/route.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import path from 'path';
-import { promises as fs } from 'fs';
-
-export async function GET() {
-  const dataDirectory = path.join(process.cwd(), 'data');
-  const fileContents = await fs.readFile(path.join(dataDirectory, 'readme.json'), 'utf8');
-  return NextResponse.json(JSON.parse(fileContents));
-}
