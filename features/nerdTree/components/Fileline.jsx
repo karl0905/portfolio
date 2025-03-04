6855d8571e12554211d1ca93b8d02a6eccec36ee
karl0905 <kalle44@live.dk>
2025-03-04 10:19:00 +0100

    style(fileline): make icons a little bit bigger

--- features/nerdTree/components/Fileline.jsx
+++ features/nerdTree/components/Fileline.jsx
@@ -1,11 +1,8 @@
-"use client"
-import { useEffect } from 'react';
-import { fetchSnippet } from '@/global'
-import axios from 'axios';
-import {
-  useMenuStore,
-  useContentStore
-} from '@/global';
+"use client";
+import { useEffect } from "react";
+import { fetchSnippet } from "@/global";
+import axios from "axios";
+import { useMenuStore, useContentStore } from "@/global";
 
 export function Fileline({
   content,
@@ -13,9 +10,8 @@ export function Fileline({
   hexcode,
   route,
   title,
-  onClick = () => { }
+  onClick = () => { },
 }) {
-
   const { setSelectedMenuItem, selectedMenuItem } = useMenuStore();
 
   const {
@@ -24,21 +20,21 @@ export function Fileline({
     clearSnippet,
     setSnippetFileType,
     clearSnippetFileType,
-    mainContent
+    mainContent,
   } = useContentStore();
 
   async function fetchData(name) {
     try {
-      const response = await axios.get(`/api/${route}`)
+      const response = await axios.get(`/api/${route}`);
       const items = response.data.data;
 
-      const matchedItem = items.find(item => item.name === name);
+      const matchedItem = items.find((item) => item.name === name);
 
       if (matchedItem) {
         return matchedItem;
       } else {
         console.log("no matchedItem with name", name);
-    console.log("content", mainContent)
+        console.log("content", mainContent);
         return null;
       }
     } catch (error) {
@@ -62,46 +58,36 @@ export function Fileline({
   };
 
   return (
-    < >
-      {
-        type === 'dir'
-          ? (
-            <p className="text-md text-[#7394d5]">
-              {content}
-            </p>
-          )
-          : type === 'file'
-            ? (
-              <
-                div className=""
-                onClick={onClick}
-              >
-                {content.data.map((item, index) => (
-                  <div
-                    key={index}
-                    className={`w-full cursor-pointer flex gap-2 whitespace-nowrap pl-8
-                ${selectedMenuItem?.name === item.name ? 'bg-[#303030]' : 'hover:bg-[#303030]'}`}
-                    onClick={() => handleMenuClick(item, route)}
-                  >
-                    <p className='text-md'>{item?.icon} {item.name}</p>
-                    {title === "experience" && (
-                      <>
-                        <p className='text-md text-[var(--orange)]'>@</p>
-                        <p className='text-md'>{item.company}</p>
-                      </>
-                    )}
-                  </div>
-                ))}
+    <>
+      {type === "dir" ? (
+        <p className="text-md text-[#7394d5]">{content}</p>
+      ) : type === "file" ? (
+        <div className="" onClick={onClick}>
+          {content.data.map((item, index) => (
+            <div
+              key={index}
+              className={`w-full cursor-pointer flex gap-2 whitespace-nowrap pl-8
+                ${selectedMenuItem?.name === item.name ? "bg-[#303030]" : "hover:bg-[#303030]"}`}
+              onClick={() => handleMenuClick(item, route)}
+            >
+              <div className="flex gap-2 items-center">
+                <p className="text-xl">{item?.icon}</p>
+                <p className="text-md">{item.name}</p>
               </div>
-            )
-            : type === 'misc'
-              ? (
-                <p className="text-md" style={{ color: `#${hexcode}` }}>
-                  {content}
-                </p>
-              )
-              : null
-      }
+              {title === "experience" && (
+                <>
+                  <p className="text-md text-[var(--orange)]">@</p>
+                  <p className="text-md">{item.company}</p>
+                </>
+              )}
+            </div>
+          ))}
+        </div>
+      ) : type === "misc" ? (
+        <p className="text-md" style={{ color: `#${hexcode}` }}>
+          {content}
+        </p>
+      ) : null}
     </>
   );
 }
